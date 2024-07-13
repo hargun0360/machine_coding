9e8da8725adfe22e0ffa7f11fd5b195d742900fb
hargun0360 <hargunsingh0720@gmail.com>
2024-07-13 16:09:45 +0530

    Custom Modal implement

--- src/App.js
+++ src/App.js
@@ -1,37 +1,86 @@
-import React , {useState} from "react";
-
-function Section({ title, content , isVisible , setIsVisible }) {
-  
-  const handleClick = () => {
-    setIsVisible(title);
-  };
-
-  return (
-    <>
-      <div
-        style={{
-          height: 'fit-content',
-          width: "100vw",
-          border: "2px solid black",
-          padding: "5px",
-          cursor: "pointer",
-        }}
-        onClick={handleClick}
-      >
-        <h1>{title}</h1>
-        {isVisible == title ? <p>{content}</p> : null}
-      </div>
-    </>
-  );
-}
+import React, { useState } from "react";
 
 function App() {
-  const [isVisible , setIsVisible] = useState("accordian 1");
+  const [isShow, setIsShow] = useState(false);
+  const [accept, setIssAccept] = useState(false);
+
+
+  // out side modal close effect 
+  const handleClick = (e) => {
+    console.log(e.target.className);
+    if(e.target.className === "App"){
+      setIsShow(false);
+    }
+  }
+
   return (
-    <div className="App">
-      <Section title="accordian 1" content="this is the first section" isVisible = {isVisible} setIsVisible = {setIsVisible} />
-      <Section title="accordian 2" content="this is the second section" isVisible = {isVisible} setIsVisible = {setIsVisible} />
-      <Section title="accordian 3" content="this is the third section" isVisible = {isVisible} setIsVisible = {setIsVisible} />
+    <div
+      className="App"
+      onClick={handleClick}
+      style={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: isShow ? "rgb(169, 169, 169)" : "transparent",
+      }}
+    >
+      {!isShow && (
+        <div className="show-offer">
+          <button
+            className="show-btn"
+            style={{ cursor: "pointer" }}
+            onClick={() => {
+              setIsShow((prev) => !prev);
+            }}
+          >
+            {accept ? "Offer Accepted" : "Show Offer"}
+          </button>
+        </div>
+      )}
+
+      {isShow && (
+        <div
+          className="modal"
+          
+          style={{
+            width: "250px",
+            height: "250px",
+            display: "flex",
+            justifyContent: "center",
+            flexDirection: "column",
+            alignItems: "center",
+            border: "1px solid black",
+            backgroundColor: "white",
+            position: "relative",
+          }}
+        >
+          <div
+            className="x"
+            style={{
+              position: "absolute",
+              left: 10,
+              top: 10,
+              cursor: "pointer",
+            }}
+            onClick={() => setIsShow(false)}
+          >
+            X
+          </div>
+          <p>Hey Hargun, Here is the Div</p>
+          <button
+            className="accept-offer"
+            style={{ cursor: "pointer" }}
+            onClick={() => {
+              setIssAccept((prev) => !prev);
+              setIsShow(false);
+            }}
+          >
+            Accept Offer
+          </button>
+        </div>
+      )}
     </div>
   );
 }
